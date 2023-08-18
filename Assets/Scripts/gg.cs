31f108d4a2be65f46cc75e4846d62931f0fbfd78
Adity-a <adityabalodi31@gmail.com>
2023-08-18 08:25:03 +0530

    First Commit

---
+++ Assets/Scripts/gg.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gg : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+	public void changeScene(string SceneName){
+		SceneManager.LoadScene(SceneName,LoadSceneMode.Single);
+	}
+}
